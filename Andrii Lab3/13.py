5682646b44649b55bf55d441db394ac719b19ad2
AndriiYefremov <90155784+AndriiYefremov@users.noreply.github.com>
2021-09-27 14:57:14 +0300

    Update 13.py

--- Andrii Lab3/13.py
+++ Andrii Lab3/13.py
@@ -1,19 +1,22 @@
+def encrypt():
+    strings = input('Введіть повідомлення: ')
+    even = strings[::2]
+    odd = strings[1::2]
+    print("Ваше зашифроване повідомлення: ", even + odd)
+
 def decrypt():
-    string=input('Введіть секретне повідомлення: ')
-    length=len(string)#для отримання довжини введення 
-    half_length=(length+1)//2 #половина вхідних даних 
-    even=string[:half_length]#парна частина
-    odd=string[half_length:]#непарна частина
-#тут ми починаємо вставляти кожну частину, щоб сформувати оригінал
-    msg=''
-    if length%2==0:
-        for i in range (half_length):
-            join=even[i]+odd[i]
-            msg=msg+join
-    else:
-        for i in range (1,half_length+1):
-            start=i-1
-            join=even[start:i]+odd[start:i]
-            msg=msg+join
-    print(msg)
+    strings = input('Введіть зашифроване повідомлення: ')
+    length = len(strings)
+    half_length = (length + 1) // 2
+    even = strings[:half_length]
+    odd = strings[half_length:]
+    res = ''
+    for i in range(half_length-1):
+        join = even[i] + odd[i]
+        res += join
+    res += even[half_length-1:]
+    res += odd[half_length - 1:]
+    print("Ваше розшифроване повідомлення: ", res)
+
 decrypt()
+encrypt()
