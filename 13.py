0bc3b5b308543e78d071fce491a3a9fc551bf463
AndriiYefremov <90155784+AndriiYefremov@users.noreply.github.com>
2021-09-27 12:59:02 +0300

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,6 +1,22 @@
 def encrypt():
-    strings=input('Введіть повідомлення: ')
-    even=strings[::2]
-    odd=strings[1::2]
-    print(even+odd)
-encrypt() 
+    strings = input('Введіть повідомлення: ')
+    even = strings[::2]
+    odd = strings[1::2]
+    print("Ваше зашифроване повідомлення: ", even + odd)
+
+def decrypt():
+    strings = input('Введіть зашифроване повідомлення: ')
+    length = len(strings)
+    half_length = (length + 1) // 2
+    even = strings[:half_length]
+    odd = strings[half_length:]
+    res = ''
+    for i in range(half_length-1):
+        join = even[i] + odd[i]
+        res += join
+    res += even[half_length-1:]
+    res += odd[half_length - 1:]
+    print("Ваше розшифроване повідомлення: ", res)
+
+decrypt()
+encrypt()
