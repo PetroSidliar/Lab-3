02ef3173c8f36098c9ad136578279eef6a675b98
OrestNadopta <90180542+OrestNadopta@users.noreply.github.com>
2021-09-27 15:27:32 +0300

    Update 6zx.py

--- Orest Lab3/6zx.py
+++ Orest Lab3/6zx.py
@@ -10,7 +10,7 @@ n = int(input('Enter a number to tell how precise the calculation has to be :'))
 def calc(n):
     e = 0
     for i in range(1, n+1):
-        e += 1/factorial(n)
+        e += 1/factorial(i)
     return e
 
 print(calc(n))
